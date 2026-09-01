b5d25178133d716c0100831da4dc4a25ffb301dc
Rayat <rayatrahman101@gmail.com>
2026-09-02 01:57:11 +0600

    some nav changes

--- cheque_requests.php
+++ cheque_requests.php
@@ -200,9 +200,9 @@ if ($conn->connect_error) {
         </div>
         <div>
             <a href="admin_dashboard.php">Dashboard</a>
-            <a href="#">Customers</a>
+            <a href="admin_customers.php">Customers</a>
             <a href="admin_accounts.php">Accounts</a>
-            <a href="#">Transactions</a>
+            <a href="admin_transactions.php">Transactions</a>
             <a href="admin_loans.php">Loans</a>
             <a href="admin_employees.php">Employees</a>
             <a href="fraud_monitoring.php">Fraud Monitoring</a>
